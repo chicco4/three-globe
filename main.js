617a540db9e4e315e143f09973066b8dff012695
chicco4 <chiccotto4@gmail.com>
2022-07-27 09:10:05 +0200

    ok check todo

--- main.js
+++ main.js
@@ -10,13 +10,21 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 //create sphere
 const geometry = new THREE.SphereGeometry(5, 50, 50);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({
+  //color: 0x00ff00,
+  map: new THREE.TextureLoader().load("./assets/earth.jpg"),
+  ////maybe jpeg is better:
+  //map: new THREE.TextureLoader().load("./assets/globe.jpeg"),
+});
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
@@ -25,8 +33,8 @@ camera.position.z = 12;
 function animate() {
   requestAnimationFrame(animate);
 
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
+  sphere.rotation.x += 0.001;
+  sphere.rotation.y += 0.001;
 
   renderer.render(scene, camera);
 }
